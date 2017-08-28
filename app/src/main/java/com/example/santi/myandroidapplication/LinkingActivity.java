30ad75e584f8082db01ecf7bde25d985becd2c17
svfranco-pw <santiago.franco@pointwest.com.ph>
2017-08-28 17:18:42 +0800

    Merge remote-tracking branch 'origin/master'
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ app/src/main/java/com/example/santi/myandroidapplication/LinkingActivity.java
@@ -0,0 +1,15 @@
+package com.example.santi.myandroidapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LinkingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linking);
+
+        getSupportActionBar().setTitle("Add Item");
+    }
+}
--- app/src/main/java/com/example/santi/myandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/santi/myandroidapplication/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.santi.myandroidapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    //public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("My List");
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        //startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch(item.getItemId()) {
+            case R.id.add:
+                intent = new Intent(this, LinkingActivity.class);
+                //EditText editText = (EditText) findViewById(R.id.editText);
+                //String message = editText.getText().toString();
+                //intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+                return(true);
+            case R.id.edit:
+                intent = new Intent(this, DisplayMessageActivity.class);
+                startActivity(intent);
+                return(true);
+            case R.id.delete:
+                //Toast.makeText(this, R.string.about_toast, Toast.LENGTH_LONG).show();
+                return(true);
+        }
+            return(super.onOptionsItemSelected(item));
     }
 }
