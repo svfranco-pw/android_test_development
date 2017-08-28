c2ab4de26faa2a0fd53571bd112e071ee38bbd2f
svfranco-pw <santiago.franco@pointwest.com.ph>
2017-08-29 00:48:39 +0800

    Code changes: Android dev
    Added code changes for the ff:
    - List view items in the Main Activity
    - Added click item listener for the List view
    - Added functionality to FAB
    - Create activity for the List view (Item info)

---
+++ app/src/main/java/com/example/santi/myandroidapplication/ItemInfoActivity.java
@@ -0,0 +1,16 @@
+package com.example.santi.myandroidapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Santi on 8/29/2017.
+ */
+
+public class ItemInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_info);
+    }
+}
--- app/src/main/java/com/example/santi/myandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/santi/myandroidapplication/MainActivity.java
@@ -6,24 +6,69 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     //public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-
+    private ListView mainListView;
+    private ArrayAdapter<String> listAdapter ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("My List");
+
+
+        /*// storing string resources into Array
+        String[] teams = getResources().getStringArray(R.array.teams);
+
+        // Binding resources Array to ListAdapter
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.textview, teams));
+
+        ListView lv = getListView();*/
+
+        //listView = (ListView) findViewById(R.id.textview);
+
+        // Find the ListView resource.
+        mainListView = (ListView) findViewById( R.id.mainListView );
+
+        // Create and populate a List of planet names.
+        String[] planets = new String[] { "Mercury", "Venus", "Earth" };
+        ArrayList<String> planetList = new ArrayList<String>();
+        planetList.addAll( Arrays.asList(planets) );
+
+        // Create ArrayAdapter using the planet list.
+        listAdapter = new ArrayAdapter<String>(this, R.layout.list_item, planetList);
+
+        // Set the ArrayAdapter as the ListView's adapter.
+        mainListView.setAdapter( listAdapter );
+
+        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Selected item
+                String team = ((TextView) view).getText().toString();
+
+                Intent intent = new Intent(getApplicationContext(), ItemInfoActivity.class);
+                intent.putExtra("team", team);
+                startActivity(intent);
+            }
+        });
     }
 
     /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+    public void addItem(View view) {
+        Intent intent;
+        intent = new Intent(this, LinkingActivity.class);
         //EditText editText = (EditText) findViewById(R.id.editText);
         //String message = editText.getText().toString();
         //intent.putExtra(EXTRA_MESSAGE, message);
-        //startActivity(intent);
+        startActivity(intent);
     }
 
     @Override
