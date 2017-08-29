4a4786ccae6564f9dac0d753b0d3b68ba20e3448
svfranco-pw <santiago.franco@pointwest.com.ph>
2017-08-29 20:52:33 +0800

    Added new activity
    Added new activity (Edit Item activity)

---
+++ app/src/main/java/com/example/santi/myandroidapplication/EditItemInfoActivity.java
@@ -0,0 +1,17 @@
+package com.example.santi.myandroidapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by franco.santiago on 8/29/2017.
+ */
+
+public class EditItemInfoActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceBundle) {
+        super.onCreate(savedInstanceBundle);
+        setContentView(R.layout.activity_edit_item_info);
+    }
+}
